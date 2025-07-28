a7b5d8aa52cd3af09d890a8ea664fd54baca40d6
rifayet <rifayetrimon88@gmail.com>
2025-07-28 16:57:22 +0800

    add footer

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Banner from "@/components/home/Banner";
 import About from "@/components/home/About";
 import Menu from "@/components/home/Menu";
 import Soalan from "@/components/home/Soalan";
+import Footer from "@/components/home/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Menu />
       <Soalan />
+      <Footer />
     </>
   );
 }
--- components/home/Footer.jsx
+++ components/home/Footer.jsx
@@ -1,3 +1,93 @@
+import Image from "next/image";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="bg-[#164776] text-white py-10 px-6">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-start gap-8">
+        {/* Left: Image */}
+        <div className="flex justify-center md:justify-start">
+          <Image
+            src="/images/banner/icon.png"
+            alt="Footer Icon"
+            width={150}
+            height={150}
+            className="object-contain"
+          />
+        </div>
+
+        {/* Right: Line + Text Content */}
+        <div className="flex w-full md:w-auto gap-6 items-start md:items-stretch justify-between md:justify-start">
+          {/* Vertical Line */}
+          <div className="hidden md:block w-[2px] bg-white" />
+
+          {/* Text & Social Icons */}
+          <div className="text-left">
+            <h3 className="text-2xl font-semibold">
+              Majlis Agama Islam Selangor (MAIS)
+            </h3>
+
+            <p className="text-base text-gray-200 mt-4 leading-relaxed">
+              Urus setia Jawatankuasa Bahagian <br />
+              Pengurusan Masjid
+            </p>
+
+            <p className="text-base text-gray-200 mt-4 leading-relaxed">
+              Jabatan Agama Islam Selangor, Aras 7, <br />
+              Menara Selatan, Bangunan Sultan Idris Shah, <br />
+              40000, Shah Alam, Selangor
+            </p>
+
+            <a
+              href="tel:+60355143512"
+              className="block text-base text-gray-200 mt-4"
+            >
+              Phone Number : +603-5514 3512 / 3513
+            </a>
+            <a
+              href="mailto:tauliah@mais.gov.my"
+              className="block text-base text-gray-200 mt-1"
+            >
+              e-mel : tauliah@mais.gov.my
+            </a>
+
+            {/* Social Icons */}
+            <div className="flex gap-5 mt-5 text-xl">
+              <a href="#" className="hover:text-gray-300" aria-label="Facebook">
+                <Image
+                  src="/icons/fb.svg"
+                  alt="Facebook"
+                  width={24}
+                  height={24}
+                  className="hover:opacity-80 transition-opacity"
+                />
+              </a>
+              <a href="#" className="hover:text-gray-300" aria-label="Twitter">
+                <Image
+                  src="/icons/instagram.svg"
+                  alt="Instagram"
+                  width={24}
+                  height={24}
+                  className="hover:opacity-80 transition-opacity"
+                />
+              </a>
+              <a
+                href="#"
+                className="hover:text-gray-300"
+                aria-label="Instagram"
+              >
+                <Image
+                  src="/icons/x.svg"
+                  alt="x"
+                  width={24}
+                  height={24}
+                  className="hover:opacity-80 transition-opacity"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
